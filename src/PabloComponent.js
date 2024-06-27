ce373fa05b53c4492c2b1ff7fe082a8e1c8ac85f
Dramzii <dramzii23@gmail.com>
2024-06-26 21:01:03 -0600

    Componente de Pablo

---
+++ src/PabloComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PabloComponent = () => {
+  return (
+    // This is the component that will be rendered in the App.js file
+    <h1>Hello from Pablo Component</h1>
+  );
+};
+
+export default PabloComponent;
